c74d2c8327da23109ada02091ba64fc208fc802f
BlackRabbit98 <sdibbya@gmail.com>
2021-09-30 16:45:04 +1000

    Add covid cases LineGraph

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,21 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 
-const LineGraph = () => {
+const LineGraph = ({ casesType = 'cases' }) => {
 	const [data, setData] = useState({});
 
+	const buildChartData = (data, casesType = 'cases') => {
+		const chartData = [];
+		let lastDataPoint;
+		for (let date in data.cases) {
+			if (lastDataPoint) {
+				let newDataPoint = {
+					x: date,
+					y: data[casesType][date] - lastDataPoint,
+				};
+				chartData.push(newDataPoint);
+			}
+			lastDataPoint = data[casesType][date];
+		}
+		return chartData;
+	};
+
 	useEffect(() => {
-		fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-			})
-			.catch((error) => console.log(error));
-	}, []);
+		const fetchData = async () => {
+			await fetch(
+				'https://disease.sh/v3/covid-19/historical/all?lastdays=120'
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					let chartData = buildChartData(data);
+					setData(chartData);
+				})
+				.catch((error) => console.log(error));
+		};
+		fetchData();
+	}, [casesType]);
 
 	return (
 		<div>
-			<Line />
+			{data?.length > 0 && (
+				<Line
+					data={{
+						datasets: [
+							{
+								backgroundColor: 'rgba(204, 16, 52, 0.5)',
+								borderColor: '#CC1034',
+								data: data,
+							},
+						],
+					}}
+				/>
+			)}
 		</div>
 	);
 };
